86d5a74968c5705ff04eb0ebdca6f2b0f95dab13
Bertan <Bertan@DESKTOP-V8BQLBS>
2024-11-12 17:42:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+class Program
+{
+    static int TunnelLenght(int TrainLenght, int Speed, int Time)
+    {
+        int IzminataDyljina = Speed * Time;
+        return IzminataDyljina - TrainLenght;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Въведете дължина на влака (метра)");
+        int TrainLength = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Въведете скорост на влака (m/s)");
+        int Speed = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Въведете време (секунди)");
+        int Time = Convert.ToInt32(Console.ReadLine());
+        int TL = TunnelLenght(TrainLength, Speed, Time);
+
+        Console.Write("Lenght of the tunnel is " + TL + " metres.");
+        
+    }
+}
